b712569218c6edc5493e8e69e67a7e1b498fe7cd
pedrobarcas <pedrobarcas.git@gmail.com>
2025-10-19 17:27:10 -0300

    Updates

---
+++ assets/main-4Exn7QGF.js
@@ -0,0 +1,1 @@
+import{h as e,c as o}from"./h-CDZiPmD0.js";import{H as r}from"./header-B6AbCCu0.js";const t=e(r,{title:"Configurações",href:o.get("home")}),n=document.getElementById("container");n.prepend(t);
